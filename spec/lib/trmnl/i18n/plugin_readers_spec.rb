3c09951a71c2457ff151376cf86d61a4cef6cab2
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-18 14:33:45 -0700

    Added locale feature specs
    Necessary to ensure we have parity with English (default) translations by looping through all files, flattening the keys, and checking for any differences. Zero differences is good because that means we have identical keys. A separate spec is supplied for category of locale despite some duplication in setup which seems acceptable for now.
    
    In order to check key parity, the locale of each file loaded is always ignored in order to ensure equal comparisons.

---
+++ spec/lib/trmnl/i18n/plugin_readers_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe "Plugin Renders", type: :feature do
+  using Refinements::Hash
+  using Refinements::Pathname
+
+  let(:default) { YAML.safe_load(root.join("en.yml").read).fetch("en").flatten_keys.keys }
+  let(:root) { Bundler.root.join "lib/trmnl/i18n/locales/plugin_renders" }
+
+  Bundler.root.join("lib/trmnl/i18n/locales/plugin_renders").files("*.yml").each do |path|
+    it "verifies #{path.name} has key parity with default locale" do
+      locale = path.name.to_s.then { |key| key == "en.raw" ? "raw" : key }
+      current = YAML.safe_load(path.read).fetch(locale).flatten_keys.keys
+
+      expect((default - current)).to eq([])
+    end
+  end
+end
