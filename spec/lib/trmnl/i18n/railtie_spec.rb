7b333f6bc3ce2f8912d2e800dbfa25e73bd0dc81
Brooke Kuhlmann <brooke@alchemists.io>
2025-02-19 15:01:11 -0700

    Added spec for available locales
    Provides an additional test to check if available locales are properly set. The `web_ui` folder is used as the source of truth.

--- spec/lib/trmnl/i18n/railtie_spec.rb
+++ spec/lib/trmnl/i18n/railtie_spec.rb
@@ -3,15 +3,27 @@
 require "spec_helper"
 
 RSpec.describe TRMNL::I18n::Railtie do
+  using Refinements::Pathname
+
   before { described_class.initializers.each(&:run) }
 
   describe ".initializer" do
-    it "loads custom locales" do
+    it "loads locales" do
       expect(I18n.load_path).to include(
         %r(.+locales/custom_plugins.+),
         %r(.+locales/plugin_renders.+),
         %r(.+locales/web_ui.+)
       )
+    end
+
+    it "answers available locales" do
+      proof = Bundler.root
+                     .join("lib/trmnl/i18n/locales/web_ui")
+                     .files("*.yml")
+                     .map { |path| path.name.to_s.to_sym }
+                     .sort
+
+      expect(I18n.available_locales.sort).to eq(proof)
     end
   end
 end
