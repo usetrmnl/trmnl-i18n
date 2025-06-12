c791520281abd0ad626d4181ed079c7e83f6c172
Ryan Kulp <ryanckulp@gmail.com>
2025-06-12 10:36:25 -0400

    Added test to check for locale_name uniqueness

--- spec/lib/trmnl/i18n/web_ui_spec.rb
+++ spec/lib/trmnl/i18n/web_ui_spec.rb
@@ -8,11 +8,19 @@ RSpec.describe "Web UI", type: :feature do
 
   let(:default) { YAML.safe_load(root.join("en.yml").read).fetch("en").flatten_keys.keys }
   let(:root) { Bundler.root.join "lib/trmnl/i18n/locales/web_ui" }
+  let(:locales) { root.files "*.yml" }
 
   Bundler.root.join("lib/trmnl/i18n/locales/web_ui").files("*.yml").each do |path|
     it "verifies #{path.name} has key parity with default locale" do
       current = YAML.safe_load(path.read).fetch(path.name.to_s).flatten_keys.keys
       expect(default - current).to eq([])
     end
+  end
+
+  it "verifies locales have unique locale_name" do
+    locale_names = locales.map do |path|
+      YAML.safe_load(path.read).fetch(path.name.to_s)["locale_name"]
+    end
+    expect(locale_names.uniq.count).to eql locales.count
   end
 end
